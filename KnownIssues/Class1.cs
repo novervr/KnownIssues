e4d212941288d6de6fedffab02e3e4aa784e5807
nover <109939195+novervr@users.noreply.github.com>
2023-06-27 11:28:28 -0500

    Update Class1.cs

--- KnownIssues/Class1.cs
+++ KnownIssues/Class1.cs
@@ -16,12 +16,11 @@ namespace KnownIssues
         private const string modName = "KnownIssues";
         private const string modVersion = "1.0.0";
 
-        static string textURL = "";
-        static string finishText;
 
         // When the script is started do this \\
         public void Awake()
         {
+            Debug.Log("Mod has been sucessfully read by BepInEx!");
             // Harmony Patches \\
             var harmony = new Harmony(modGUID);
             harmony.PatchAll(Assembly.GetExecutingAssembly());
@@ -38,11 +37,14 @@ namespace KnownIssues
                     for (int i = 0; i < GorillaComputer.instance.levelScreens.Length; i++)
                     {
                         Material colorr = new Material(Shader.Find("Standard"));
-                        colorr.color = Color.black;
+                        colorr.color = Color.cyan;
+                        // Find all text elements in Level/lower level/UI for Code of Conduct and the color for \\
                         GameObject.Find("Level/lower level/StaticUnlit/screen").GetComponent<Renderer>().material = colorr;
-                        GameObject.Find("Level/lower level/UI/CodeOfConduct").GetComponent<Text>().text = "[<color=yellow>KNOWN ISSUES</color>]";
-                        GameObject.Find("Level/lower level/UI/CodeOfConduct/COC Text").GetComponent<Text>().text = finishText;
+                        GameObject.Find("Level/lower level/UI/CodeOfConduct").GetComponent<Text>().text = "[<color=yellow>KNOWN ISSUES MOD</color>]";
+                        GameObject.Find("Level/lower level/UI/CodeOfConduct/COC Text").GetComponent<Text>().text = "THE CURRENT ISSUE FOR THIS MOD IS GETTING INFO FROM A GITHUB PAGE. PLEASE WAIT FOR V1.0.1 UNTIL THIS IS FIXED. THANKS";
                     }
+
+
                 }
                 catch
                 {
